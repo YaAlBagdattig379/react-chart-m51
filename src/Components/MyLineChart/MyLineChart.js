c185f048c5ff9a1cc22ebf4c2a7993b82f6e36f7
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2022-12-23 11:43:13 +0530

    added about of chart normal sample

--- src/Components/MyLineChart/MyLineChart.js
+++ src/Components/MyLineChart/MyLineChart.js
@@ -1,55 +1,59 @@
 import React from 'react';
-import { LineChart,Line } from 'recharts';
+import { LineChart,Line,Tooltip,XAxis,YAxis } from 'recharts';
 
 const MyLineChart = () => {
     
 const data = [
     {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
+      name: 'supplier A',
+      price: 4000,
+      sales: 2400,
       amt: 2400,
     },
     {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
+      name: 'supplier B',
+      price: 3000,
+      sales: 1398,
       amt: 2210,
     },
     {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
+      name: 'supplier C',
+      price: 2000,
+      sales: 9800,
       amt: 2290,
     },
     {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
+      name: 'supplier D',
+      price: 2780,
+      sales: 3908,
       amt: 2000,
     },
     {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
+      name: 'supplier E',
+      price: 1890,
+      sales: 4800,
       amt: 2181,
     },
     {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
+      name: 'supplier F',
+      price: 2390,
+      sales: 3800,
       amt: 2500,
     },
     {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
+      name: 'supplier G',
+      price: 3490,
+      sales: 4300,
       amt: 2100,
     },
   ];
     return (
-        <LineChart width={400} height={400} data={data}>
-              <Line dataKey="amt"></Line>
+        <LineChart width={800} height={500} data={data}>
+              <Line dataKey="price"stroke="gold"></Line>
+              <Line dataKey="sales"stroke="purple"></Line>
+              <XAxis dataKey={'name'}/>
+              <YAxis />
+              <Tooltip></Tooltip>
         </LineChart>
             
     );
